920451b06f962f66e92c5d83af2ef5a46899ea9e
Alexander Gorokhov <ag250652@ncr.com>
2023-06-11 17:50:53 -0300

    Security login support, token evaluation, adding filters, temporary disable cors

--- src/main/java/org/challenge/config/SecurityConfig.java
+++ src/main/java/org/challenge/config/SecurityConfig.java
@@ -1,21 +1,44 @@
 package org.challenge.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.challenge.service.UserService;
+import org.challenge.util.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
+@Slf4j
 public class SecurityConfig {
 
     UserService userDetailsService;
@@ -41,25 +64,86 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
             // disabling csrf since we won't use form login
             .csrf().disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             // giving permission to every request for /login endpoint
-            .authorizeRequests().antMatchers("/api/v1/user/login").permitAll()
+            .authorizeRequests()
+            .antMatchers("/api/v1/user/login").permitAll()
             // for everything else, the user has to be authenticated
             .anyRequest().authenticated()
+            .and()
+            .headers().frameOptions().disable() // to enable h2-console
             // setting stateless session, because we choose to implement Rest API
-            .and().sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            .and()
+            .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         // adding the custom filter before UsernamePasswordAuthenticationFilter in the filter chain
-      //  http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        //  http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
+    // Configure CORS globally
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // Allow requests from any origin
+        configuration.addAllowedMethod("*"); // Allow all methods
+        configuration.addAllowedHeader("*"); // Allow all headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
     @Bean
     AuthenticationManager authenticationManager(
         AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    private class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+        //
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+            try {
+                // TODO test
+
+
+                if (isLoginRequest(request)) {
+                    chain.doFilter(request, response);
+                    return;
+                }
+                String token;
+                if(request.getHeader("Authorization") != null) {
+                    token = request.getHeader("Authorization").replace("Bearer ", "");
+                } else {
+                    throw new JwtException("No token found");
+                }
+                    // Validate and parse the token
+                    Claims claims = TokenUtil.parseToken(token);
+
+                    // Token is valid, create an authentication object
+                    String username = claims.getSubject();
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (JwtException e) {
+                // Token is invalid or expired, log the error
+                log.error("Invalid token", e);
+                throw e;
+            }
+
+            chain.doFilter(request, response);
+        }
+    }
+
+
+    private boolean isLoginRequest(HttpServletRequest request) {
+        return request.getMethod().equals("POST") && request.getRequestURI().equals("/api/v1/user/login");
     }
 }
