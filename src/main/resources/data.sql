1cc250e5edb0c6b26ece5e9e4c4b556c38f5b1de
Alexander Gorokhov <ag250652@ncr.com>
2023-06-11 17:51:37 -0300

    Intial data load

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,17 @@
-INSERT INTO users (id, username, password, status, created_at)
-VALUES (1,'test@domain.com','$2a$10$RMvB0po4bpJGwpcyX20c1u1NmIgl0uUd0t9UWPXwerd2W/nPuruxe','ACTIVE','2023-06-06 12:00:00');
+INSERT INTO users (id, username, password, status, balance, created_at)
+VALUES (1,'test@domain.com','$2a$10$RMvB0po4bpJGwpcyX20c1u1NmIgl0uUd0t9UWPXwerd2W/nPuruxe','ACTIVE',10.0,'2023-06-06 12:00:00');
 
 INSERT INTO operation (id, type,cost, created_at)
 VALUES (0,'INIT','0','2023-06-06 12:00:00');
 INSERT INTO operation (id, type,cost, created_at)
 VALUES (1,'ADDITION','1.0','2023-06-06 12:00:00');
-
-INSERT INTO record (id, operation_id, user_id, amount, user_balance, operation_response, operation_date, created_at)
-VALUES ('e0e465c4-320c-4df1-82e3-71248bea8f81',0,1,0,10.0,'PERFORMED','2023-06-06 12:00:00','2023-06-06 12:00:00');
+INSERT INTO operation (id, type,cost, created_at)
+VALUES (2,'SUBTRACTION','1.0','2023-06-06 12:00:00');
+INSERT INTO operation (id, type,cost, created_at)
+VALUES (3,'MULTIPLICATION','1.0','2023-06-06 12:00:00');
+INSERT INTO operation (id, type,cost, created_at)
+VALUES (4,'DIVISION','1.0','2023-06-06 12:00:00');
+INSERT INTO operation (id, type,cost, created_at)
+VALUES (5,'SQUARE_ROOT','1.0','2023-06-06 12:00:00');
+INSERT INTO operation (id, type,cost, created_at)
+VALUES (6,'RANDOM_STRING','5.0','2023-06-06 12:00:00');
